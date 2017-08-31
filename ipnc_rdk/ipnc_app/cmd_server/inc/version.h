ada12c9004a8076c117e679661ba75292a42f0da
pamsimochen <82425509@qq.com>
2017-08-31 20:58:19 +0800

    a beta version with ubifs

--- ipnc_rdk/ipnc_app/cmd_server/inc/version.h
+++ ipnc_rdk/ipnc_app/cmd_server/inc/version.h
@@ -1,6 +1,6 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define VERSION_NUMBER "r55M 804cdda"
+#define VERSION_NUMBER "r55+4 0488596"
 
 #endif // VERSION_H
--- ipnc_rdk/ipnc_app/root_filesys/opt/ipnc/autorun.sh
+++ ipnc_rdk/ipnc_app/root_filesys/opt/ipnc/autorun.sh
@@ -1,14 +1,11 @@
 #./boa -c /etc &
-#/usr/sbin/avahi-autoipd --no-drop-root --no-chroot --force-bind -D eth0
-#/usr/sbin/avahi-daemon --no-drop-root -D
-#/usr/sbin/avahi-dnsconfd -D
-#touch /tmp/sem_start 
-#sleep 1
-#./system_server &
-#sleep 1
-#./boot_proc 1
-#sleep 1
-#./cmd_server &
+sleep 1
+./cmd_server &
+
+/usr/sbin/avahi-autoipd --no-drop-root --no-chroot --force-bind -D eth0
+/usr/sbin/avahi-daemon --no-drop-root -D
+/usr/sbin/avahi-dnsconfd -D
+touch /tmp/sem_start 
 
 #ethtool -C eth0 tx-usecs 500
 #ethtool -C eth0 rx-usecs 500
